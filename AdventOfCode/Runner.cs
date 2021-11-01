dfd419a97c3187fe9a00dfdf6ad577b4b8babc49
Jared Yost <jaredyost@me.com>
2021-10-31 19:38:22 -0500

    Initial Commit and 2020/Day1

---
+++ AdventOfCode/Runner.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.y2020;
+using System;
+
+namespace AdventOfCode
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Code...\n\n");
+
+            // TODO: Add a selector here
+            {
+                ReportRepair reportRepair = new ReportRepair();
+                int result = reportRepair.Calculate2Entries();
+                if(result != (int)ReportRepair.ErrorCodes.INVALID_RESULT)
+                {
+                    Console.WriteLine($"RESULT (2 Entires): { result }");
+                }
+                else
+                {
+                    Console.WriteLine("Result not found.");
+                }
+            }
+
+            {
+                ReportRepair reportRepair = new ReportRepair();
+                int result = reportRepair.Calculate3Entries();
+                if(result != (int)ReportRepair.ErrorCodes.INVALID_RESULT)
+                {
+                    Console.WriteLine($"RESULT (3 Entires): { result }");
+                }
+                else
+                {
+                    Console.WriteLine("Result not found.");
+                }
+            }
+
+            /* Wait for an enter key press to exit */
+            Console.ReadLine();
+        }
+    }
+}
