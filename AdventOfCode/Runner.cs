50339d4ddb757d4aaeb8c7da2bfed97c4244033b
Jared Yost <jaredyost@me.com>
2022-12-01 23:27:02 -0600

    Add 2022/Day2

--- AdventOfCode/Runner.cs
+++ AdventOfCode/Runner.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             Console.WriteLine("~~ ADVENT OF CODE ~~\n");
-            new y2022.Day1().Run();
+            new y2022.Day2().Run();
         }
     }
 }
