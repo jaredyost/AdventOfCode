179c8c31c41d8e54c5c0094e0c815953e1cdceed
Jared Yost <jaredyost@me.com>
2024-12-03 17:50:54 -0600

    Add 2024 Day 3
    Add the solver for 2024 day 3.

---
+++ Tests/Y2024/Day03Tests.cs
@@ -0,0 +1,68 @@
+using AdventOfCode.Solvers.Y2024;
+
+namespace AdventOfCode.Tests.Y2024
+{
+    [TestClass]
+    public class Day03Tests
+    {
+        [TestMethod]
+        public async Task Y2024_D03_Part1_Example()
+        {
+            // Arrange
+            Day03 solver = new();
+            string[] TestInput =
+            [
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            ];
+
+            // Act
+            string result = await solver.SolvePart1(TestInput);
+
+            // Assert
+            Assert.AreEqual("161", result);
+        }
+
+        [TestMethod]
+        public async Task Y2024_D03_Part2_Example()
+        {
+            // Arrange
+            Day03 solver = new();
+            string[] TestInput =
+            [
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            ];
+
+            // Act
+            string result = await solver.SolvePart2(TestInput);
+
+            // Assert
+            Assert.AreEqual("48", result);
+        }
+
+        [TestMethod]
+        public async Task Y2024_D03_Part1_Real()
+        {
+            // Arrange
+            Day03 solver = new();
+
+            // Act
+            string result = await solver.SolvePart1(solver.ProblemInput);
+
+            // Assert
+            Assert.AreEqual("163931492", result);
+        }
+
+        [TestMethod]
+        public async Task Y2024_D03_Part2_Real()
+        {
+            // Arrange
+            Day03 solver = new();
+
+            // Act
+            string result = await solver.SolvePart2(solver.ProblemInput);
+
+            // Assert
+            Assert.AreEqual("76911921", result);
+        }
+    }
+}
