7be747a96855e0488232e43f9f2aff2f79a0a8d4
Jared Yost <jaredyost@me.com>
2025-12-21 19:55:48 -0600

    Add Remaining Day 8 Solution
    Bring in the rest of the code needed to solve day 8.

--- Solvers/Y2025/Day08.cs
+++ Solvers/Y2025/Day08.cs
@@ -1,9 +1,5 @@
 using System.Numerics;
 using Connection = AdventOfCode.Core.Helpers.UnorderedPair<System.Numerics.Vector3>;
-using ConnectionData = System.Collections.Generic.KeyValuePair<
-    AdventOfCode.Core.Helpers.UnorderedPair<System.Numerics.Vector3>,
-    float
->;
 
 namespace AdventOfCode.Solvers.Y2025
 {
@@ -15,7 +11,75 @@ namespace AdventOfCode.Solvers.Y2025
 
         public override ValueTask<string> SolvePart1(string[] aInput)
         {
-            List<Vector3> junctionBoxes =
+            // Get the sorted junction box connections
+            Vector3[] junctionBoxes = GetJunctionBoxes(aInput);
+            Connection[] sortedConnections = GetSortedConnections(junctionBoxes);
+
+            // Seed the circuits with the junction boxes
+            List<List<Vector3>> circuits = [];
+            foreach (Vector3 junctionBox in junctionBoxes)
+            {
+                circuits.Add(new([junctionBox]));
+            }
+
+            // Connect together the specified number of junction boxes
+            for (int i = 0; i < NumberPairs; i++)
+            {
+                Connection connection = sortedConnections[i];
+                List<Vector3> circuit1 = circuits.First(x => x.Contains(connection.Item1));
+                List<Vector3> circuit2 = circuits.First(x => x.Contains(connection.Item2));
+
+                if (circuit1 != circuit2)
+                {
+                    circuit1.AddRange(circuit2);
+                    circuits.Remove(circuit2);
+                }
+            }
+
+            // Sort the circuits based off most connections
+            circuits = [.. circuits.OrderByDescending(x => x.Count)];
+
+            // Utilize the 3 longest circuits
+            return new((circuits[0].Count * circuits[1].Count * circuits[2].Count).ToString());
+        }
+
+        public override ValueTask<string> SolvePart2(string[] aInput)
+        {
+            // Get the sorted junction box connections
+            Vector3[] junctionBoxes = GetJunctionBoxes(aInput);
+            Connection[] sortedConnections = GetSortedConnections(junctionBoxes);
+
+            // Seed the circuits with the junction boxes
+            List<List<Vector3>> circuits = [];
+            foreach (Vector3 junctionBox in junctionBoxes)
+            {
+                circuits.Add(new([junctionBox]));
+            }
+
+            // Connect together all of the junction boxes
+            Connection lastConnection = sortedConnections[0];
+            for (int i = 0; circuits.Count > 1; i++)
+            {
+                Connection connection = sortedConnections[i];
+                List<Vector3> circuit1 = circuits.First(x => x.Contains(connection.Item1));
+                List<Vector3> circuit2 = circuits.First(x => x.Contains(connection.Item2));
+
+                if (circuit1 != circuit2)
+                {
+                    lastConnection = connection;
+                    circuit1.AddRange(circuit2);
+                    circuits.Remove(circuit2);
+                }
+            }
+
+            // Utilize the X-coordinate of the last connection made
+            ulong result = (ulong)lastConnection.Item1.X * (ulong)lastConnection.Item2.X;
+            return new(result.ToString());
+        }
+
+        private static Vector3[] GetJunctionBoxes(string[] aInput)
+        {
+            return
             [
                 .. aInput
                     .Select(x => x.Split(','))
@@ -25,13 +89,17 @@ namespace AdventOfCode.Solvers.Y2025
                         float.Parse(x[2])
                     )),
             ];
+        }
 
+        private static Connection[] GetSortedConnections(Vector3[] aJunctionBoxes)
+        {
+            // Calculate the distance between every junction box
             Dictionary<Connection, float> connections = [];
-            for (int i = 0; i < junctionBoxes.Count; i++)
+            for (int i = 0; i < aJunctionBoxes.Length; i++)
             {
-                for (int j = i + 1; j < junctionBoxes.Count; j++)
+                for (int j = i + 1; j < aJunctionBoxes.Length; j++)
                 {
-                    Connection connection = new(junctionBoxes[i], junctionBoxes[j]);
+                    Connection connection = new(aJunctionBoxes[i], aJunctionBoxes[j]);
                     if (!connections.ContainsKey(connection))
                     {
                         connections.Add(
@@ -41,21 +109,9 @@ namespace AdventOfCode.Solvers.Y2025
                     }
                 }
             }
-            List<ConnectionData> sortedConnections = [.. connections.OrderBy(x => x.Value)];
 
-            List<List<ConnectionData>> circuits = [];
-            for (int i = 0; i < NumberPairs; i++)
-            {
-                // TODO: Link up the junction boxes
-            }
-            circuits = [.. circuits.OrderByDescending(x => x.Count)];
-
-            return new((circuits[0].Count * circuits[1].Count * circuits[2].Count).ToString());
-        }
-
-        public override ValueTask<string> SolvePart2(string[] aInput)
-        {
-            return new(0.ToString());
+            // Sort the connections based off short distance
+            return [.. connections.OrderBy(x => x.Value).Select(x => x.Key)];
         }
     }
 }
